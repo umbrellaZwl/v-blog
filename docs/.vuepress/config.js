33a83fbc7754de93d5bad5219115ea43532a1474
zhangwenliang <zhangwenliang@qipeng.com>
2020-08-19 14:09:37 +0800

    feat: 修改目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,13 +5,13 @@ module.exports = {
   themeConfig: {
     sidebar: [
       '/',
-      '/vscode搭配linter&prettier',
-      '/常用代码片段',
-      '/浏览器进程和js线程',
-      '/script标签的async和deffer',
-      '/fetch',
-      '/Http1.0、Http1.1、Http2.0',
-      '/vue的keep-alive'
+      // '/vscode搭配linter&prettier',
+      // '/常用代码片段',
+      // '/浏览器进程和js线程',
+      // '/script标签的async和deffer',
+      // '/fetch',
+      // '/Http1.0、Http1.1、Http2.0',
+      // '/vue的keep-alive'
     ]
   }
 }
--- docs/README.md
+++ docs/README.md
@@ -2,3 +2,15 @@
 
 ## 代码规范篇
 [vscode搭配linter&prettier](vscode搭配linter&prettier.md)
+## 常用代码
+[常用代码片段](常用代码片段.md)
+## vue
+[vue的keep-alive](vue的keep-alive.md)
+## 碎片积累
+[浏览器进程和js线程](浏览器进程和js线程.md)
+
+[script标签的async和deffer](script标签的async和deffer.md)
+
+[Http1.0、Http1.1、Http2.0](Http1.0、Http1.1、Http2.0.md)
+
+[fetch](fetch.md)
